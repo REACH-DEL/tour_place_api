bb577e85c183294404ee23092e90acd0632b1e9e
REACH-DEL <tirachlo34@gmail.com>
2025-12-10 09:27:11 +0700

    remove unuse file

--- migration_add_profile_image.sql
+++
@@ -1,5 +0,0 @@
--- Migration script to add profile_image column to users table
--- Run this if you have an existing database without the profile_image column
-
-ALTER TABLE users ADD COLUMN IF NOT EXISTS profile_image TEXT;
-
